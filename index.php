ec2808221cc2120dbc8d452328c077c246414f7f
Jackeline963 <jackelinedecarmena@gmail.com>
2019-04-03 15:18:03 -0600

    modales de registro e ingreso

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html> 
 <html>
 <head>
+<meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!--responsive -->
 
 <link rel="stylesheet" type="text/css" href="bootstrap3/css/bootstrap.min.css">
@@ -58,8 +59,10 @@
             <li><a href="#">Otro</a></li>
             <li role="separator" class="divider"></li>
           </ul>
-        </li>
       </ul>
+
+     
+            
       <!--<form class="navbar-form navbar-left">
         <div class="form-group">
           <input type="text" class="form-control" placeholder="Search">
@@ -67,18 +70,14 @@
         <button type="submit" class="btn btn-default">Submit</button>
       </form>-->
       <ul class="nav navbar-nav navbar-right">
-       
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Salir <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Cerrar sesión</a></li>
-          </ul>
-        </li>
-      </ul>
+      <li class="active"><a href="#" data-toggle="modal" data-target="#ingresarmodal">Registrase</a></li>
+      <li class="active"><a href="#" data-toggle="modal" data-target="#loginmodal" >LogIn</a></li>
+      <li class="active"><a href="#"><i class="fa fa-fw fa-power-off"></i></a></li>
+
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
-     <!--<img src="public/img/logouls.png" alt="logouls" class="center" style="width:20%;"> -->
+     
      </div>
 </div>
 
@@ -94,15 +93,15 @@
     <!-- Wrapper for slides -->
     <div class="carousel-inner">
       <div class="item active">
-        <img src="public/img/01.jpeg" alt="" style="width:65%;">
+        <img src="public/img/01.jpeg" alt="" style = "width: 700px; height:400px;">
       </div>
 
       <div class="item">
-        <img src="public/img/02.jpg" alt="" style="width:65%;">
+        <img src="public/img/02.jpg" alt="" style = "width: 700px; height: 400px;">
       </div>
     
       <div class="item">
-       < <img src="public/img/03.jpg" alt="" style="width:65%;">
+        <img src="public/img/03.jpg" alt="" style = "width: 700px; height: 400px;">
       </div>
     </div>
 
@@ -117,6 +116,94 @@
     </a>
   </div>
 </div>
+
+<!--Modal de Log In-->
+ <div class="container" style="margin-top: 60px;">
+    <!--button class="btn btn-info" data-toggle="modal" data-target="#miformulario">
+    LOGIN
+    </button-->
+
+    <div class=" modal fade" id="loginmodal" tabindex="-1" role="dialog" aria-labelledby="loginmodal1" aria hidden="true">
+      <div class=" modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true"> X </button>
+            <h4>LOGIN</h4>
+          </div>
+
+            <div class="col-xs-12">
+              <label>Usuario</label>
+              <input type="text" class="form-control" placeholder="Escriba su usuario">
+            </div>
+            <div class="col-xs-12">
+              <label>Contraseña</label>
+              <input type="text" class="form-control" placeholder="Escriba su contraseña">
+            </div>
+            <div class="modal-footer">
+            <button type="button" class="btn btn-success btn-block"  data-dismiss="modal">ingresar</button>
+            </div>
+        </div>
+        </div>     
+    </div>
+    </div>
+<!--Fin Modal de Log In-->
+<!--Inicio Modal de crear cuenta-->
+ <div class="container" style="margin-top: 60px;">
+   
+
+    <div class=" modal fade" id="ingresarmodal" tabindex="-1" role="dialog" aria-labelledby="ingresarmodal0" aria hidden="true">
+      <div class=" modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true"> X </button>
+            <h4>CREAR CUENTA </h4>
+          </div>
+
+          <div class="modal-body">
+           <div class="row">
+            <div class="col-xs-6">
+              <label>Nombres</label>
+              <input type="text" class="form-control" placeholder="Escriba sus nombres">
+            </div>
+
+            <div class="col-xs-6">
+              <label>Apellidos</label>
+              <input type="text" class="form-control" placeholder="Escriba sus apellidos">
+            </div>
+          
+            <div class="col-xs-12">
+              <label>E-mail</label>
+              <input type="text" class="form-control" placeholder="Escriba su correo">
+            </div>
+            <div class="col-xs-12">
+              <label>Categoría</label>
+              <input type="text" class="form-control" placeholder="Seleccione una categoría">
+            </div>
+            <div class="col-xs-12">
+              <label>Usuario</label>
+              <input type="text" class="form-control" placeholder="usuario">
+            </div>
+            <div class="col-xs-12">
+              <label>Contraseña</label>
+              <input type="text" class="form-control" placeholder="Escriba su contraseña">
+            </div>
+            <div class="col-xs-12">
+              <label>Contraseña</label>
+              <input type="text" class="form-control" placeholder="Confirme su contraseña">
+            </div>
+            </div>
+           </div>
+            <div class="modal-footer">
+            <button type="button" class="btn btn-success btn-block"  data-dismiss="modal"><span class="glyphicon glyphicon-floppy-disk"></span> Cerrar</button>
+            </div>
+          
+        </div>
+        </div>
+      </div>
+    </div> 
+ 
+
+<!--Fin Modal de crear cuenta-->
 </body>
 </html>
 
